71f7e9230fa46e7b1a5d93675e21609ee7795608
Siranjeevi029 <siranjeevi73389@gmail.com>
2025-09-07 22:00:02 +0530

    Update CorsConfig.java
    cors

--- backend-auth/src/main/java/com/telusko/part29springsecex/config/CorsConfig.java
+++ backend-auth/src/main/java/com/telusko/part29springsecex/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3000","https://authorization-frontend-yui1.onrender.com"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
